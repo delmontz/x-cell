a853c0957ba47d6db8117e67cb6dba52822b3c1b
delmont <pazxx219@gmail.com>
2018-05-05 20:11:25 +0900

    レッスン4-21まで完了＆lint設定見直し

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,16 +7,31 @@ module.exports = {
         // browser グローバル変数を使用する
         "browser": true,
     },
+    "globals": {
+        "window": true,
+        "module": true
+    },
     "rules": {
-        // インデントスタイルは2スペースに強制
+        // インデントスタイルは3スペースに強制
         "indent": ["error", 3],
         // 改行コードはWindows
         "linebreak-style": ["error", "windows"],
-        //未定義が鬱陶しいのでとりあえず無効　追加
-        "no-unused-vars": 1,
-        "require-jsdoc" : 0,
-        "space-before-blocks": 0,
-        "keyword-spacing" :0,
-        "spaced-comment": 0
-    },
+        //演算子の前後にはスペースを強制
+        "space-infix-ops": "error",
+        //宣言されていない変数の使用を禁止
+        "no-undef": "error",
+        //アロー関数の矢印の前後にスペースを強制
+        "arrow-spacing": "error",
+        //未使用変数
+        "no-unused-vars": "warn",
+        //条件式で代入
+        "no-cond-assign": "error",
+        //keyの重複
+        "no-dupe-keys": "error",
+        /*** 鬱陶しいのでとりあえず無効追加 ***/
+        "require-jsdoc" : "off",
+        "space-before-blocks": "off",
+        "keyword-spacing" : "off",
+        "spaced-comment": "off"
+    }
   };
---
+++ client/js/app.js
@@ -0,0 +1,5 @@
+/* eslint-disable */
+let test = () => {
+   
+}
+let obj = {bar: 'unko', bar: 'unko'};
